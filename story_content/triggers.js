4dbf885d8f1f3025be7398e8c0b975d5327da9ba
Desartam <designerart@mail.com>
2024-10-02 13:53:05 -0400

    10-02-2024 new

---
+++ story_content/triggers.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6q9vuTosvoG":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('65M8mPZ12Pe');
+const duration = 2250;
+const easing = 'ease-out';
+const id = '5v9AoE7WoJ6';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
